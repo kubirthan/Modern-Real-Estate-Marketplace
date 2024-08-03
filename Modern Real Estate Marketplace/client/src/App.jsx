1d9b46562ccc1d7f34e547a2588317446e2839b6
kubirthan <kubirthan7991@gmail.com>
2024-08-03 11:38:37 +0530

    create and run the server

---
+++ Modern Real Estate Marketplace/client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Header from './components/Header'
+
+export default function App() {
+  return (
+  <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+    </Routes>
+  </BrowserRouter>
+  )
+}
