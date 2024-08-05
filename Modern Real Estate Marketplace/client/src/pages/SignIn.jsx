57a271274e3014dc6b626dc5d9059441e136431f
kubirthan <kubirthan7991@gmail.com>
2024-08-05 12:53:31 +0530

    Add redux-toolkit

--- Modern Real Estate Marketplace/client/src/pages/SignIn.jsx
+++ Modern Real Estate Marketplace/client/src/pages/SignIn.jsx
@@ -1,58 +1,72 @@
-import React, { useState } from 'react'
-import {Link, useNavigate} from 'react-router-dom'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
 
 export default function SignIn() {
-  const [formData, setFormData] = useState({})
-  const [error, setError] = useState(null)
-  const [loading, setLoading] = useState(false)
-
-  const navigate = useNavigate()
-
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.id]: e.target.value
-    })
-  }
-
+      [e.target.id]: e.target.value,
+    });
+  };
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true)
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
-      method: 'POST',
-      headers: {
-        'Content-type':'application/json',
-      },
-      body: JSON.stringify(formData)
-    }) 
-    const data = await res.json()
-    if(data.success === false) {
-      setLoading(false)
-      setError(data.message)
-      return
-    }
-    setLoading(false)
-    setError(null)
-    navigate('/')
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+        return;
+      }
+      dispatch(signInSuccess(data));
+      navigate('/');
     } catch (error) {
-      setLoading(false)
-      setError(error.message)
+      dispatch(signInFailure(error.message));
     }
-    
-  }
-
-
-
+  };
   return (
-    <div className=' p-3 max-w-lg mx-auto'>
+    <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input type='email' placeholder='email' className='border p-3 rounded-lg ' id='email' onChange={handleChange}/>
-        <input type='password' placeholder='password' className='border p-3 rounded-lg ' id='password' onChange={handleChange}/>
-        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+        <input
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
+          onChange={handleChange}
+        />
+
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
           {loading ? 'Loading...' : 'Sign In'}
         </button>
+        <OAuth/>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Dont have an account?</p>
@@ -60,6 +74,7 @@ export default function SignIn() {
           <span className='text-blue-700'>Sign up</span>
         </Link>
       </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
-  )
+  );
 }
---
+++ Modern Real Estate Marketplace/client/src/redux/store.js
@@ -0,0 +1,10 @@
+import {configureStore} from '@reduxjs/toolkit'
+import { useReducer } from 'react'
+
+export const store = configureStore({
+    reducer: {user: useReducer},
+    middleware: (getDefaultMiddleware) => 
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
+})
