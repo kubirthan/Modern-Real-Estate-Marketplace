911ed79a5d8bd48680e5e1b85eb1d5e8d0d2d9da
kubirthan <kubirthan7991@gmail.com>
2024-08-05 11:20:04 +0530

    complete signin page functionality

--- Modern Real Estate Marketplace/client/src/pages/SignUp.jsx
+++ Modern Real Estate Marketplace/client/src/pages/SignUp.jsx
@@ -19,7 +19,7 @@ export default function SignUp() {
     e.preventDefault()
     try {
       setLoading(true)
-    const res = await fetch('/api/auth/signup', {
+      const res = await fetch('/api/auth/signup', {
       method: 'POST',
       headers: {
         'Content-type':'application/json',
