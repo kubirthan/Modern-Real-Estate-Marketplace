2587a99b2a9eebe2fdd40fe8ca505c0e11b0a3c6
kubirthan <kubirthan7991@gmail.com>
2024-08-06 19:16:04 +0530

    Update the header and make the profile page private

--- Modern Real Estate Marketplace/client/src/components/Header.jsx
+++ Modern Real Estate Marketplace/client/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { FaSearch } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import {useSelector} from 'react-redux'
 
 
 export default function Header() {
+    const {currentUser} = useSelector(state => state.user)
     return (
         <header className='bg-slate-200 shadow-md'>
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -27,8 +29,12 @@ export default function Header() {
                         <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
 
                     </Link>
-                    <Link to='/sign-in'>
+                    <Link to='/profile'>
+                    {currentUser ? (
+                        <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile'/>
+                    ) : (
                         <li className=' text-slate-700 hover:underline'>Sign in</li>
+                    )}
                     </Link>
                 </ul>
             </div>
